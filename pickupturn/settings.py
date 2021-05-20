90dd487136d5fbebf7fc0c1a9664f505c3c7e871
Farid Wajdi <faridwajdi05@gmail.com>
2021-05-20 22:37:47 +0700

    Move config to OS environment

--- pickupturn/settings.py
+++ pickupturn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%nf8ck%1^c56@8zil)x-msec+y@-lhr&8+&f7159qx*8y9$4@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
